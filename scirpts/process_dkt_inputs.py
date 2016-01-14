814d9e61085f771d7c71082c6fe9a3aeecd001e7
Xiaolu Xiong <beardeer@gmail.com>
2016-01-14 14:57:39 -0500

    update script

--- scirpts/process_dkt_inputs.py
+++ scirpts/process_dkt_inputs.py
@@ -10,8 +10,9 @@ import csv
 print 'start'
 
 
-input_file = open('..\local_data\skill_builder_data_good.csv', 'rb')
-output_file = open('..\local_data\dkt_input.txt', 'w')
+input_file = open('..\data\skill_builder_data_good.csv', 'rb')
+output_file = open('..\data\dkt_input.txt', 'w')
+output_file_2 = open('..\data\dkt_input_2.txt', 'w')
 csv_reader = csv.reader(input_file)
 print csv_reader.next()
 
@@ -22,6 +23,7 @@ skill_count = 111
 csv_data = []
 skill_skill_mapping = {}
 current_str = ''
+current_str_2 = ''
 current_skill_count = -1
 
 current_user_id = None
@@ -37,20 +39,31 @@ for row in csv_reader:
         skill_skill_mapping[skill_id] = current_skill_count
     skill_id = skill_skill_mapping[skill_id]
     
-    current_coding[correct * skill_count + skill_id] = '1'
+    
+    skill_coding = correct * skill_count + skill_id
+    current_coding[skill_coding] = '1'
     
     if user_id != current_user_id and current_user_id != None:
         current_str = current_str[:-1] + '\n'
         output_file.write(current_str)
+
+        current_str_2 = current_str_2[:-1] + '\n'
+        output_file_2.write(current_str_2)
+        
         current_str = ''
+        current_str_2 = ''
         
     current_str = current_str + ''.join(current_coding) + ','
+    current_str_2 = current_str_2 + str(skill_coding) + ','
+    
     current_user_id = user_id
 
 
 current_str = current_str[:-1] + '\n'
+current_str_2 = current_str_2[:-1] + '\n'
+
 output_file.write(current_str)
-current_str = ''
+output_file_2.write(current_str_2)
 
 input_file.close()
 output_file.close()
